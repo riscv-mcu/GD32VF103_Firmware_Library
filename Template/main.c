9592ed5c5007ebd2e0458b7e1a3d28d5410bc5df
Kongou Hikari <hikari@iloli.bid>
2019-09-02 14:34:26 +0800

    Templates: Add Makefile

--- Template/main.c
+++ Template/main.c
@@ -48,12 +48,11 @@ int main(void)
     gd_eval_led_init(LED1);
     gd_eval_led_init(LED2);
     gd_eval_led_init(LED3);
-    gd_eval_led_init(LED4);
 
     while(1){
         /* turn on led1, turn off led4 */
         gd_eval_led_on(LED1);
-        gd_eval_led_off(LED4);
+        gd_eval_led_off(LED3);
         delay_1ms(1000);
         /* turn on led2, turn off led1 */
         gd_eval_led_on(LED2);
@@ -62,10 +61,6 @@ int main(void)
         /* turn on led3, turn off led2 */
         gd_eval_led_on(LED3);
         gd_eval_led_off(LED2);
-        delay_1ms(1000);
-        /* turn on led4, turn off led3 */
-        gd_eval_led_on(LED4);
-        gd_eval_led_off(LED3);
         delay_1ms(1000);
     }
 }
