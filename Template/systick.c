4e35a8535d15e31c5b3a2390f189d26581ad2bf3
Kongou Hikari <hikari@iloli.bid>
2019-09-02 14:47:26 +0800

    Template: Optimize for code size

--- Template/systick.c
+++ Template/systick.c
@@ -43,15 +43,19 @@ OF SUCH DAMAGE.
 */
 void delay_1ms(uint32_t count)
 {
-    uint64_t start_mtime, delta_mtime;
+    volatile uint64_t start_mtime, delta_mtime;
 
-    // Don't start measuruing until we see an mtime tick
-    uint64_t tmp = get_timer_value();
+    volatile uint64_t tmp = get_timer_value();
     do {
-    start_mtime = get_timer_value();
-    } while (start_mtime == tmp);
+    	start_mtime = get_timer_value();
+    } while(start_mtime == tmp);
+
+
+    uint64_t delay_ticks = SystemCoreClock / 4; // 1 second
+    delay_ticks = delay_ticks * count / 1000;
 
     do {
-    delta_mtime = get_timer_value() - start_mtime;
-    }while(delta_mtime <(SystemCoreClock/4000.0 *count ));
+    	delta_mtime = get_timer_value() - start_mtime;
+    } while(delta_mtime < delay_ticks);
+
 }
